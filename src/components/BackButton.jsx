f4cdde6bfc93c35324d10d19f15861121d1e91c3
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2025-02-26 06:45:52 +0700

    feat: separate BackButton into its own component

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
+const BackButton = ({ to = "/" }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      data-svg-wrapper
+      className="left-[10%] md:left-[90px] top-[16%] absolute cursor-pointer transition-all duration-300 ease-in-out hover:scale-110 hover:opacity-80"
+      onClick={() => navigate(to)}
+    >
+      <svg
+        width="80"
+        height="80"
+        viewBox="0 0 118 118"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="md:w-[118px] md:h-[118px]"
+      >
+        <g filter="url(#filter0_dd_2129_60850)">
+          <rect x="28" y="18" width="62" height="62" rx="31" fill="white" />
+          <path
+            d="M68.159 48.9975H50.5454M50.5454 48.9975L59 40.543M50.5454 48.9975L59 57.4521"
+            stroke="#004F52"
+            strokeWidth="2.11364"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default BackButton;
--- src/components/BannerProduct.jsx
+++ src/components/BannerProduct.jsx
@@ -1,40 +1,14 @@
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import BackButton from "./BackButton";
 
 const BannerProduct = ({ image: ProductImage, title, description }) => {
-  const navigate = useNavigate();
-
   return (
     <div className="w-full max-w-[1441px] h-[538px] relative">
       {/* Background utama */}
       <div className="w-full h-full absolute bg-[#178488]" />
 
-      {/* Tombol Back (menggunakan SVG) */}
-      <div
-        data-svg-wrapper
-        className="left-[10%] md:left-[117px] top-[16%] absolute cursor-pointer transition-all duration-300 ease-in-out hover:scale-110 hover:opacity-80"
-        onClick={() => navigate("/")}
-      >
-        <svg
-          width="80"
-          height="80"
-          viewBox="0 0 118 118"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="md:w-[118px] md:h-[118px]"
-        >
-          <g filter="url(#filter0_dd_2129_60850)">
-            <rect x="28" y="18" width="62" height="62" rx="31" fill="white" />
-            <path
-              d="M68.159 48.9975H50.5454M50.5454 48.9975L59 40.543M50.5454 48.9975L59 57.4521"
-              stroke="#004F52"
-              strokeWidth="2.11364"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </g>
-        </svg>
-      </div>
+      {/* Tombol Back */}
+      <BackButton to="/" />
 
       {/* Kotak kuning sebagai container teks */}
       <div className="w-[90%] max-w-[1036px] h-[93px] left-[5%] md:left-[195px] top-[45%] absolute bg-[#f1b434] rounded-[10px] flex items-center justify-start px-6">
