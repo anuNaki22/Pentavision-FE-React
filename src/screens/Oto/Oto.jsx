2d0050e95e9909f971d97b9038c31022c49e6c2b
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2025-02-26 05:50:44 +0700

    style: adjust layout to implement overlay appearance

--- src/screens/Oto/Oto.jsx
+++ src/screens/Oto/Oto.jsx
@@ -2,27 +2,35 @@ import Accordion from "../../components/Accordion";
 import PriceForm from "../../components/PriceForm";
 import BannerProduct from "../../components/BannerProduct";
 import ProductOto from "../../assets/product-oto.svg?react";
+import Card from "../../components/Card";
+import CardFinancing from "../../components/CardFinancing";
 
 const Oto = () => {
   return (
-    <div className="space-y-4">
-      <BannerProduct
-        image={ProductOto}
-        title="OTO BSI"
-        description="Setir Kendaraan Impian dengan Skema Syariah!"
-      />
-      <Accordion
-        title="Syarat & Ketentuan"
-        content={
-          <ul className="list-disc pl-4 md:pl-6 space-y-2 md:space-y-3">
-            <li>Data Diri (KTP, KK, NPWP)</li>
-            <li>PBB</li>
-            <li>Slip Gaji 3 bulan terakhir/Surat keterangan Kerja</li>
-            <li>Rekening koran 3 bulan terakhir</li>
-            <li>Materai 10.000 (4 lembar)</li>
-          </ul>
-        }
-      />
+    <div className="space-y-4 pb-32">
+      <div className="relative z-0">
+        <BannerProduct
+          image={ProductOto}
+          title="OTO BSI"
+          description="Setir Kendaraan Impian dengan Skema Syariah!"
+        />
+      </div>
+      <div className="-mt-28 mb-16 relative z-10">
+        <Accordion
+          title="Syarat & Ketentuan"
+          content={
+            <ul className="list-disc pl-4 md:pl-6 space-y-2 md:space-y-3">
+              <li>Data Diri (KTP, KK, NPWP)</li>
+              <li>PBB</li>
+              <li>Slip Gaji 3 bulan terakhir/Surat keterangan Kerja</li>
+              <li>Rekening koran 3 bulan terakhir</li>
+              <li>Materai 10.000 (4 lembar)</li>
+            </ul>
+          }
+        />
+      </div>
+      {/* <Card />
+      <CardFinancing /> */}
       <PriceForm title="atau, Kamu Bisa Input Harga Sendiri Disini!" />
     </div>
   );
