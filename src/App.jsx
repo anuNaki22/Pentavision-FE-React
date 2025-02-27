74eab1d8687950d3b19c2d562cbdf379a3237a6e
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2025-02-27 13:28:43 +0700

    fix: delete form path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Homescreen/Home";
-import Form from "./screens/Form/Form";
 import Griya from "./screens/Griya/Griya";
 import Oto from "./screens/Oto/Oto";
 import Mitraguna from "./screens/Mitraguna/Mitraguna";
@@ -11,7 +10,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} key="home"></Route>
-        <Route path="/form" element={<Form />} key="form"></Route>
         <Route path="/griya" element={<Griya />} key="griya"></Route>
         <Route path="/oto" element={<Oto />} key="oto"></Route>
         <Route
