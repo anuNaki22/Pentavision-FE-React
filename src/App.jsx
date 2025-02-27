5b1493257cd725be509a42a3fa26c373bceba652
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2025-02-28 00:25:08 +0700

    feat: add scroll to top useeffect component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Griya from "./screens/Griya/Griya";
 import Oto from "./screens/Oto/Oto";
 import Mitraguna from "./screens/Mitraguna/Mitraguna";
 import Predict from "./screens/Predict/Predict";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} key="home"></Route>
         <Route path="/griya" element={<Griya />} key="griya"></Route>
