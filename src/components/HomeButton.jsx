e1866cf6fc0b412833b5dfad3d39c698bd8d26a7
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2025-02-28 08:26:10 +0700

    style: improve layouting

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -10,58 +10,28 @@ const HomeButton = ({ to = "/", className = "" }) => {
       onClick={() => navigate(to)}
     >
       <svg
-        width="240"
-        height="240"
-        viewBox="0 0 240 240"
+        width="62"
+        height="62"
+        viewBox="89 70 62 62"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g filter="url(#shadow)">
-          <rect x="89" y="70" width="62" height="62" rx="31" fill="white" />
-          <path
-            d="M116.204 112.388H113.674C110.878 112.388 108.612 110.122 108.612 107.327V99.3648C108.612 97.595 109.537 95.9537 111.05 95.0364L117.377 91.2021C118.989 90.225 121.011 90.225 122.623 91.2021L128.95 95.0364C130.463 95.9537 131.388 97.595 131.388 99.3648V107.327C131.388 110.122 129.122 112.388 126.327 112.388H123.796M116.204 112.388V107.327C116.204 105.23 117.904 103.531 120 103.531V103.531C122.096 103.531 123.796 105.23 123.796 107.327V112.388M116.204 112.388H123.796"
-            stroke="#004F52"
-            strokeWidth="1.89796"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
         <defs>
-          <filter
-            id="shadow"
-            x="85"
-            y="66"
-            width="70"
-            height="70"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dx="0" dy="4" />
-            <feGaussianBlur stdDeviation="5" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0.1 0 0 0 0 0.15 0 0 0 0 0.2 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow"
-              result="shape"
-            />
-          </filter>
+          <radialGradient id="shadowGradient" cx="50%" cy="50%" r="50%">
+            <stop offset="0%" stopColor="rgba(0, 0, 0, 0.8)" />
+            <stop offset="100%" stopColor="rgba(0, 0, 0, 0)" />
+          </radialGradient>
         </defs>
+
+        <circle cx="120" cy="101" r="36" fill="url(#shadowGradient)" />
+        <rect x="89" y="70" width="62" height="62" rx="31" fill="white" />
+        <path
+          d="M116.204 112.388H113.674C110.878 112.388 108.612 110.122 108.612 107.327V99.3648C108.612 97.595 109.537 95.9537 111.05 95.0364L117.377 91.2021C118.989 90.225 121.011 90.225 122.623 91.2021L128.95 95.0364C130.463 95.9537 131.388 97.595 131.388 99.3648V107.327C131.388 110.122 129.122 112.388 126.327 112.388H123.796M116.204 112.388V107.327C116.204 105.23 117.904 103.531 120 103.531V103.531C122.096 103.531 123.796 105.23 123.796 107.327V112.388M116.204 112.388H123.796"
+          stroke="#004F52"
+          strokeWidth="1.89796"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     </div>
   );
--- src/screens/Predict/Predict.jsx
+++ src/screens/Predict/Predict.jsx
@@ -14,7 +14,6 @@ const Predict = () => {
       window.location.href = "/"; // Redirect ke home
     } else {
       setResult(storedResult);
-      //   window.localStorage.removeItem("result"); // Hapus setelah diambil
     }
   }, []);
 
@@ -31,99 +30,97 @@ const Predict = () => {
     data_now = result.calc_result.filter(
       (data) => data.tenor === result.input_data.Duration
     );
-    console.log(data_now);
   }
 
   return (
     <>
-      <HomeButton />
-      <div className="my-0 flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center mt-20">
-          <div className="max-w-[800px] flex-col items-center text-justify self-center">
-            <h2
-              className={`text-3xl font-semibold text-center mt-28 my-2 ${
-                result.prediction_text === "APPROVED"
-                  ? "text-[#00696D]"
-                  : "text-[#ed8b00]"
-              }`}
-            >
-              {result.prediction_text === "APPROVED"
-                ? "Alhamdulillah"
-                : "Mohon Maaf"}
-            </h2>
+      <HomeButton className="top-4 left-4 md:top-12 md:left-12 lg:top-24 lg:left-24" />
+      <div className="my-0 flex flex-col items-center justify-center px-4">
+        <div className="flex flex-col items-center mt-10 md:mt-14 w-full max-w-[900px]">
+          <h2
+            className={`text-2xl md:text-4xl font-semibold text-center mt-16 md:mt-28 my-2 ${
+              result.prediction_text === "APPROVED"
+                ? "text-[#00696D]"
+                : "text-[#ed8b00]"
+            }`}
+          >
+            {result.prediction_text === "APPROVED"
+              ? "Alhamdulillah"
+              : "Mohon Maaf"}
+          </h2>
 
-            <div className="flex justify-center mb-4 text-md">
-              {result.prediction_text === "APPROVED" ? (
-                <p id="result-string">
-                  Cek detail di bawah untuk melihat pilihan tenor, margin, dan
-                  angsuran per bulan yang sesuai kebutuhanmu.
-                </p>
-              ) : (
-                <p id="result-string">
-                  Saat ini kamu belum memenuhi syarat pembiayaan. Kamu bisa cek
-                  kembali opsi lain atau{" "}
-                  <a
-                    href="mailto:bsifund@bankbsi.co.id"
-                    className="font-bold underline text-[#3E9EA2]"
-                  >
-                    hubungi kami
-                  </a>{" "}
-                  untuk solusi terbaik. InsyaAllah, ada jalan yang berkah
-                  untukmu. ✨
-                </p>
-              )}
-            </div>
-            {result.prediction_text === "APPROVED" && (
-              <>
-                <div className="w-full flex-row justify-between items-center text-center self-center flex bg-[#00696D] rounded-xl p-6 mb-2 text-white text-2xl">
-                  <div className="flex-col flex-2 items-start pl-4 space-y-6 text-start font-bold">
-                    <div>Tenor</div>
-                    {/* <div>Total Pembayaran Pertama</div> */}
-                    <div>Margin</div>
-                    <div>Angsuran per Bulan</div>
-                  </div>
-                  <div className="flex-col flex-1 items-start space-y-6 text-start font-bold">
-                    <div>{data_now[0].tenor} bulan</div>
-                    {/* <div>{formatRupiah(data_now[0].installment)}</div> */}
-                    <div>{Number(data_now[0].margin).toFixed(2)}%</div>
-                    <div>{formatRupiah(parseInt(data_now[0].installment))}</div>
-                  </div>
-                </div>
-                <div className="w-full text-[#3E9EA2] text-sm text-center">
-                  Disclaimer: Hasil di atas merupakan angka estimasi, untuk info
-                  lebih lanjut{" "}
-                  <a
-                    href="mailto:bsifund@bankbsi.co.id"
-                    className="font-bold underline"
-                  >
-                    Hubungi Kami
-                  </a>
-                  .
-                </div>
-              </>
+          <div className="flex justify-center mb-4 mt-3 text-sm md:text-xl ">
+            {result.prediction_text === "APPROVED" ? (
+              <p id="result-string">
+                Cek detail di bawah untuk melihat pilihan tenor, margin, dan{" "}
+                <br />
+                angsuran per bulan yang sesuai kebutuhanmu.
+              </p>
+            ) : (
+              <p id="result-string">
+                Saat ini kamu belum memenuhi syarat pembiayaan. Kamu bisa cek
+                kembali opsi lain atau{" "}
+                <a
+                  href="mailto:bsifund@bankbsi.co.id"
+                  className="font-bold underline text-[#3E9EA2]"
+                >
+                  hubungi kami
+                </a>{" "}
+                untuk solusi terbaik. InsyaAllah, ada jalan yang berkah untukmu.
+                ✨
+              </p>
             )}
           </div>
+
+          {result.prediction_text === "APPROVED" && (
+            <>
+              <div className="w-full flex-row justify-between items-center text-center self-center flex bg-[#00696D] rounded-xl p-4 md:p-6 mb-2 mt-8 text-white text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                <div className="flex-col flex-2 items-start pl-4 space-y-4 md:space-y-6 text-start font-bold">
+                  <div>Tenor</div>
+                  {/* <div>Total Pembayaran Pertama</div> */}
+                  <div>Margin</div>
+                  <div>Angsuran per Bulan</div>
+                </div>
+                <div className="flex-col flex-1 items-start space-y-4 md:space-y-6 text-start font-bold">
+                  <div>{data_now[0].tenor} bulan</div>
+                  {/* <div>{formatRupiah(data_now[0].installment)}</div> */}
+                  <div>{Number(data_now[0].margin).toFixed(2)}%</div>
+                  <div>{formatRupiah(parseInt(data_now[0].installment))}</div>
+                </div>
+              </div>
+
+              <div className="w-full text-[#3E9EA2] text-xs md:text-sm text-center mt-2">
+                Disclaimer: Hasil di atas merupakan angka estimasi, untuk info
+                lebih lanjut{" "}
+                <a
+                  href="mailto:bsifund@bankbsi.co.id"
+                  className="font-bold underline"
+                >
+                  Hubungi Kami
+                </a>
+                .
+              </div>
+            </>
+          )}
         </div>
 
         {result.prediction_text === "APPROVED" && (
-          <>
-            <div className="flex items-center justify-center gap-5 p-10 mb-10">
-              <p className="text-2xl font-semibold">
-                Mau lanjutkan proses pembiayaan? Yuk, download
-              </p>
-              <button
-                className=" text-white px-2 py-2 rounded-lg flex-row justify-center items-center hover:transform hover:scale-105 transition-transform duration-300"
-                onClick={() =>
-                  window.open(
-                    "https://play.google.com/store/apps/details?id=co.id.bankbsi.superapp&hl=en",
-                    "_blank"
-                  )
-                }
-              >
-                <LogoByond className="w-[90%] h-[90%] cursor-pointer" />
-              </button>
-            </div>
-          </>
+          <div className="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-5 p-6 md:p-10 mb-10">
+            <p className="text-lg md:text-2xl font-semibold text-center">
+              Mau lanjutkan proses pembiayaan? Yuk, download
+            </p>
+            <button
+              className="text-white px-2 py-2 rounded-lg flex justify-center items-center hover:scale-105 transition-transform duration-300"
+              onClick={() =>
+                window.open(
+                  "https://play.google.com/store/apps/details?id=co.id.bankbsi.superapp&hl=en",
+                  "_blank"
+                )
+              }
+            >
+              <LogoByond className="w-[80px] md:w-[120px] h-[80px] md:h-[120px] cursor-pointer" />
+            </button>
+          </div>
         )}
       </div>
     </>
